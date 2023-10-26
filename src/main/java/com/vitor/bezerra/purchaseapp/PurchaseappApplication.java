9b5ee16009674239b4d656894e63f5bfcf629f36
Vitor Bezerra <vitorhenrique908@gmail.com>
2023-10-26 19:49:32 -0300

    First commit

---
+++ src/main/java/com/vitor/bezerra/purchaseapp/PurchaseappApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.bezerra.purchaseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitor/bezerra/purchaseapp/PurchaseappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.bezerra.purchaseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
